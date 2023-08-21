6da4e56a4d0657834d64113ec1f4fffe8ece83e1
eduuwem <eduuwem.udoh@gmail.com>
2023-08-21 08:46:11 -0700

    get_width.c

---
+++ get_width.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * get_width -  this calculates and stores the minimum number
+ * of character allocate for printing
+ * @format: Formatted strings to print the arguments.
+ * @x: List of arguments to be printed.
+ * @a_list: list of arguments..
+ *
+ * Return: width.
+ */
+int get_width(const char *format, int *x, va_list a_list)
+{
+		int current_index;
+		int width = 0;
+
+		for (current_i = *x + 1; format[current_index] != '\0';
+			current_index++)
+		{
+
+			if (is_digit(format[current_index]))
+			{
+				width *= 10;
+				width += format[current_index] - '0';
+			}
+			else if (format[current_index] == '*')
+			{
+				current_index++;
+				width = va_arg(a_list, int);
+				break;
+			}
+			else
+				break;
+		}
+
+		*x = current_index - 1;
+
+		return (width);
+}
+
+
