628fd4ef964d0c9ef12e8fd1707f6a05e5316a9c
eduuwem <eduuwem.udoh@gmail.com>
2023-08-22 21:23:37 -0700

    update

--- get_size.c
+++ get_size.c
@@ -8,24 +8,13 @@
  * Return: Precision.
  */
 int get_size(const char *format, int *x)
-	/**
-	 * the function takes two parameters: format is a pointer to a
-	 * constant array(formatted string), and x is a pointer to an
-	 * integer(tracking the current_index in the format string
-	 */
 {
 	int current_index = *x + 1;
 	int size = 0;
-/**
- * if the current_index in the format string is l, the size is 
- * is assign to the value of macro S_LONG
- */
+
 	if (format[current_index] == 'l')
 		size = S_LONG;
-/**
-  * if the current_index is h, the size is assign to the value of 
-  * macro S_SHORT
- */
+
 	else if (format[current_index] == 'h')
 		size = S_SHORT;
 
